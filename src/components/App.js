ce97186ad42f16c35a934eb6a9c8421d712a22d9
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-07-02 14:19:11 -0300

    LoginScreen done

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
 import "./css/reset.css";
+import LoginScreen from "./LoginScreen";
 import SingupScreen from "./SingupScreen";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LoginScreen />} />
         <Route path="/singup" element={<SingupScreen />} />
       </Routes>
     </BrowserRouter>
